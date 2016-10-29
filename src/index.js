d839fc392bb172b8a78c332cd329a4034ea084d6
Blackrush <blackrushx@gmail.com>
2016-10-29 16:06:41 +0200

    Fix issue #7

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 import { configureStore } from './configureStore'
 
-import './scss/scims.scss';
+import './scss/scims.scss'
 
 class App extends React.Component {
   state = {
