17299b28eba0ec7012f6cff99c01e25c55b23980
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-10-30 00:47:33 +0200

    Add footer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 
 const App = ({children}) => (
   <div>
     <Header />
+
     <div className="page-wrap">
       {children}
+
+      <Footer />
     </div>
   </div>
 )
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className="footer clear-float">
+    <p>
+      SciMS &copy; 2016<br />
+      <a href="https://github.com/rouenssi-tnp">@rouenssi-tnp</a> on Github
+    </p>
+  </footer>
+)
+
+export default Footer
