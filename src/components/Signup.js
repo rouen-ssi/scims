23d0b0a95227336df63c575d609715d19104412a
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-10-29 19:09:04 +0200

    Add sign-up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,8 +3,35 @@ import React from 'react'
 class Signup extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Sign-up</h1>
+      <div className="bloc login-form">
+        <form>
+          <table>
+            <tr>
+              <td><label for="email">E-mail</label></td>
+              <td><input type="email" id="email" /></td>
+            </tr>
+
+            <tr>
+              <td><label for="last_name">Last name</label></td>
+              <td><input type="test" id="last_name" /></td>
+            </tr>
+
+            <tr>
+              <td><label for="first_name">First name</label></td>
+              <td><input type="text" id="first_name" /></td>
+            </tr>
+
+            <tr>
+              <td><label for="password">Password:</label></td>
+              <td><input type="password" id="password" /></td>
+            </tr>
+
+            <tr>
+              <td></td>
+              <td><button>Sign-up</button></td>
+            </tr>
+          </table>
+        </form>
       </div>
     )
   }
