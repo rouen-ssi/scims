ac5bbee3a4eb693f0b11591f89fdb68d5300ce1a
Mathieu Brochard <mathieu.brochard@gmx.fr>
2016-10-30 10:59:01 +0100

    Add publication date, author and share button in Article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Article = () => (
   <article className='bloc article'>
     <h1>Lorem ipsum di amet super-title</h1>
+
+    <div className='article-infos'>
+      <ul>
+        <li><i className='fa fa-calendar'></i> 20/10/2016</li>
+        <li><i className='fa fa-user'></i> Mathieu</li>
+        <li><Link to='#'><i className='fa fa-share'></i>Share</Link></li>
+      </ul>
+    </div>
 
     <h2>The standart ipsum</h2>
 
