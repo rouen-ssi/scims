b5e24451d1f2ee522531092547e1cd7bc3ca7c8a
Blackrush <blackrushx@gmail.com>
2016-11-26 12:00:45 +0100

    Improve Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,40 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
 
+const HeaderLink = ({to, icon, label}) => (
+  <span>
+    <i className={`fa fa-${icon}`}/>
+    {' '}
+    <Link to={to}>{label}</Link>
+  </span>
+)
+
 const LoggedHeader = [
-  <li key="/me"><Link to="/me">MY PROFILE</Link></li>,
-  <li key="/signout"><Link to="/signout">SIGN OUT</Link></li>,
+  <li key="/draft"><HeaderLink to="/draft" label="NEW ARTICLE" icon="plus"/></li>,
+  <li key="/me"><HeaderLink to="/me" label="MY PROFILE" icon="user-circle"/></li>,
+  <li key="/signout"><HeaderLink to="/signout" label="SIGN OUT" icon="sign-out"/></li>,
 ]
 
 const GuestHeader = [
-  <li key="/signup"><Link to="/signup">SIGN UP</Link></li>,
-  <li key="/signin"><Link to="/signin">SIGN IN</Link></li>,
+  <li key="/signup"><HeaderLink to="/signup" label="SIGN UP" icon="user-plus"/></li>,
+  <li key="/signin"><HeaderLink to="/signin" label="SIGN IN" icon="sign-in"/></li>,
 ]
 
 const Header = ({logged}) => (
   <header className='header'>
     <div className='header-wrap'>
       <h1 className='logo'>SciMS</h1>
+
       <nav className='nav'>
         <ul>
-          <li><Link to='/'>HOME</Link></li>
-          <li><a href='#'>CATEGORIES</a></li>
-          <li><a href='#'>ARCHIVES</a></li>
+          <li><HeaderLink to="/" label="HOME" icon="home"/></li>
+          <li><HeaderLink to="/" label="CATEGORIES" icon="database"/></li>
+          <li><HeaderLink to="/" label="ARCHIVES" icon="archive"/></li>
 
           { logged ? LoggedHeader : GuestHeader }
         </ul>
       </nav>
+
       <div className='clear-float'></div>
     </div>
   </header>
